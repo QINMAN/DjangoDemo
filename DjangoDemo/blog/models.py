eb44c77a49207a281a6a9bf6fe4d7ec80b924371
QINMAN <qm_199301@163.com>
2017-06-21 22:15:45 +0800

    create db

--- DjangoDemo/blog/models.py
+++ DjangoDemo/blog/models.py
@@ -1,1 +1,21 @@
 #coding:utf-8
+
+import MySQLdb
+from django.db import models
+
+class category(models.Model):
+    cid = models.AutoField(primary_key=True)
+    catename = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'category'
+
+class article(models.Model):
+    aid = models.AutoField(primary_key = True)
+    title = models.CharField(max_length=50)
+    context = models.TextField()
+    addtime = models.PositiveIntegerField(default=0)
+    uptime = models.PositiveIntegerField(default=0)
+    author = models.CharField(max_length=50)
+    cid = models.PositiveIntegerField(default=0)
+    class Meta:
+        db_table = 'article'
